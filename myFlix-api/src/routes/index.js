029e88ad72dd7604ef5748db03bf6ec7817dd888
G1tHubLloyd <jongielobo@rocketmail.com>
2025-12-04 11:39:42 -0500

    feat(api): add protected users listing endpoint

--- myFlix-api/src/routes/index.js
+++ myFlix-api/src/routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const authRouter = require('./auth');
 const moviesRouter = require('./movies');
+const usersRouter = require('./users');
 
 router.use('/auth', authRouter);
 router.use('/movies', moviesRouter);
+router.use('/users', usersRouter);
 
 module.exports = router;
